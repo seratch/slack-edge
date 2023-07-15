4e3675764cea020af1e198b758bad3d13fc9f2ae
Kazuhiro Sera <seratch@gmail.com>
2023-07-15 17:58:03 +0900

    bugfix

--- src/response/response.ts
+++ src/response/response.ts
@@ -57,6 +57,11 @@ export function toCompleteResponse(
     Object.prototype.hasOwnProperty.call(slackResponse, "response_action")
   ) {
     completeResponse = { status: 200, body: slackResponse };
+  } else if (
+    Object.prototype.hasOwnProperty.call(slackResponse, "options") ||
+    Object.prototype.hasOwnProperty.call(slackResponse, "option_groups")
+  ) {
+    completeResponse = { status: 200, body: slackResponse };
   } else {
     completeResponse = slackResponse as SlackResponse;
   }
