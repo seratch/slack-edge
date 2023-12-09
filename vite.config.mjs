5013c2f99e25585dd5a047e4db90cada98104b06
Kazuhiro Sera <seratch@gmail.com>
2023-12-09 13:18:21 +0900

    version 0.8.3

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,5 @@
-/// <reference types="vitest" />
-
 // Configure Vitest (https://vitest.dev/config/)
-
 import { defineConfig } from 'vite'
-
 export default defineConfig({
   test: {
     /* for example, use global to avoid globals imports (describe, test, expect): */
