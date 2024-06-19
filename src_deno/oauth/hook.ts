8f66da41827fcb5139d0d6d92f4aecca25cc2cd1
Kazuhiro Sera <seratch@gmail.com>
2024-06-19 12:05:33 +0900

    Update Deno source files

--- src_deno/oauth/hook.ts
+++ src_deno/oauth/hook.ts
@@ -1,4 +1,4 @@
-import { OAuthV2AccessResponse } from "https://deno.land/x/slack_web_api_client@0.12.0/mod.ts";
+import { OAuthV2AccessResponse } from "https://deno.land/x/slack_web_api_client@0.12.1/mod.ts";
 import { InvalidStateParameter, OAuthErrorCode } from "./error-codes.ts";
 import { Installation } from "./installation.ts";
 import {
