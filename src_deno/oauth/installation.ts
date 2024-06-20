4226a6f4fc776e377fa1358d39d8362d50a1a484
Kazuhiro Sera <seratch@gmail.com>
2024-06-20 09:17:01 +0900

    Upgrade slack-web-api-client to 0.13

--- src_deno/oauth/installation.ts
+++ src_deno/oauth/installation.ts
@@ -1,4 +1,4 @@
-import { OAuthV2AccessResponse } from "https://deno.land/x/slack_web_api_client@0.12.1/mod.ts";
+import { OAuthV2AccessResponse } from "https://deno.land/x/slack_web_api_client@0.13.0/mod.ts";
 
 /**
  * Slack app installation data.
--- src_deno/request/payload/block-suggestion.ts
+++ src_deno/request/payload/block-suggestion.ts
@@ -1,4 +1,4 @@
-import { AnyOption } from "https://deno.land/x/slack_web_api_client@0.12.1/mod.ts";
+import { AnyOption } from "https://deno.land/x/slack_web_api_client@0.13.0/mod.ts";
 import { DataSubmissionView } from "./view-objects.ts";
 
 /**
