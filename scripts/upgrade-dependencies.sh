f295c8e7ca565a0364f32732918bfcdac3baf5a9
Kazuhiro Sera <seratch@gmail.com>
2024-07-23 12:48:04 +0900

    version 0.14.0

--- scripts/upgrade-dependencies.sh
+++ scripts/upgrade-dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 npm i slack-web-api-client@latest
-npm i --save-dev @vitest/coverage-v8@latest prettier@latest typescript@latest vitest@latest
+npm i --save-dev @vitest/coverage-v8@latest @biomejs/biome@latest typescript@latest vitest@latest
